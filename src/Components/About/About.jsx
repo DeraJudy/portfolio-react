b9bfe3bb3507db7a9cf6c9fc9b31fac2b605302d
Chidera Ulu <chideraulu@gmail.com>
2024-09-18 17:06:38 +0100

    another commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className="about-title">
+        <h1>About Me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className='about-Sections'>
+        <div className="about-left">
+            <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+            <div className="para">
+                <p>
+                    I am an experienced Frontend Developer with over a decade of professional 
+                    expertise in the field. Throughout my career, I have had the privilege 
+                    of collaborating with prestigious organizations, contributing to their success and growth.
+                </p>
+                <p>
+                    My passion for frontend development is not only reflected in my extensive 
+                    experience but also in the enthusiasm and dedication I bring to each project.
+                </p>
+            </div>
+            <div className='about-skills'>
+                <div className='about-skill'></div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
