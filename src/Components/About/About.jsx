305c12451f219615f59aec4f5093d4da75598fd4
Chidera Ulu <chideraulu@gmail.com>
2024-09-19 23:34:39 +0100

    mywork commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
             </div>
 
             <div className='about-skill'>
-              <p>AVASCRIPT</p>
+              <p>JAVASCRIPT</p>
               <hr style={{width:"60%"}} />
             </div>
 
